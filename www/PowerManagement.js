6df424d7157c64e4edf9fde1563348befcee7b97
dnelson_simplec <dnelson@simplec.com>
2013-12-02 18:02:44 -0500

    .

--- www/PowerManagement.js
+++ www/PowerManagement.js
@@ -30,20 +30,6 @@ PowerManagement.prototype.release = function(successCallback, failureCallback) {
 	cordovaRef.exec(successCallback, failureCallback, 'PowerManagement', 'release', []);
 };
 
-/**
- * Acquire a partial wake-lock, allowing the device to dim the screen
- * 
- * @param successCallback
- *            function to be called when the wake-lock was acquired successfully
- * @param errorCallback
- *            function to be called when there was a problem with acquiring the
- *            wake-lock
- */
-PowerManagement.prototype.dim = function(successCallback, failureCallback) {
-	cordovaRef.exec(successCallback, failureCallback, 'PowerManagement', 'acquire', [ true ]);
-};
-
-
 if (cordovaRef) {
 	if(!window.plugins) {
 	    window.plugins = {};
